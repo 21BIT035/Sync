74766a681872792c373a56d997a2f1ac64cce84a
21BIT035 <santhoshkumar851976@gmail.com>
2025-05-21 12:24:24 +0530

    Initial commit structured successfully

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const syncRoutes = require('./routes/syncRoutes')
+
+const app = express();
+const PORT = 3000;
+
+app.use('/api',syncRoutes)
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
